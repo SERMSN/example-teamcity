c5979854c1d4dac6eb0cfc538a1e63983a4badd5
SerMSN <sermsn@gmail.com>
2026-01-02 17:38:44 +0700

    Добавлен метод WRCS проверка hunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-	public String sayHunter(){
-		return "This hunter is name is Sergei!";
+	public String sayWRCS(){
+		return "This hunter is name is WRCS!";
 	}
 }
